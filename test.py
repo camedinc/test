4f1c0042272463c57edc6783ada2b215bd2fc5f2
camedinc <camedinc@gmail.com>
2024-08-04 00:44:42 -0400

    first commit

---
+++ test.py
@@ -0,0 +1,17 @@
+# librerias 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# prueba
+print("Hola mundo")
+
+# operaciones
+print(np.log(5))
+
+# prueba
+print("Chao mundo")
+
+# prueba 
+print(4%3)
